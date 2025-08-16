7ac73867827079f0c953be64ee1744e64d08c03e
kaushikann <112846739+kaushikann@users.noreply.github.com>
2025-08-16 22:03:46 +0530

    Update DailyNews.py

--- DailyNews.py
+++ DailyNews.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 model = ChatOpenAI(model="gpt-4o-mini")
 from langchain.agents import create_openai_functions_agent, AgentExecutor
 from langchain import hub
-from composio_langchain import ComposioToolSet, Action, App
+from composio import ComposioToolSet, Action, App
 from agents import Agent, Runner, WebSearchTool
 import asyncio
 
@@ -39,20 +39,18 @@ if st.button("What's the recent news about AI?", type="primary"):
         st.session_state['news'] = news
         st.success("News fetched!")
 st.text_area("Latest AI News", st.session_state['news'], height=300)            
-st.write("Give your email address to send this news to your inbox. We do not store your email address!")
-email = st.text_input("Enter your email")
+
 if st.button("Send this news via Email"):
     if st.session_state['news']:
-        with st.spinner("Sending email..."):
-            try:
-                Email_Tool(st.session_state['news'],email)
-                st.success("Email sent!")
-            except Exception as e:
-                st.error(f"Failed to send email: {e}")
+        st.write("Give your email address to send this news to your inbox. We do not store your email address!")
+        email = st.text_input("Enter your email")
+        if email:
+            with st.spinner("Sending email..."):
+                try:
+                    Email_Tool(st.session_state['news'], email)
+                    st.success("Email sent!")
+                except Exception as e:
+                    st.error(f"Failed to send email: {e}")
     else:
         st.warning("Please fetch the news first.")
-
-
-
-
 
